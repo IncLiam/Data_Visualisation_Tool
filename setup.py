501298179c4c8f6885417aae8606b2a314a6ed64
liampgrichardson <80628115+liampgrichardson@users.noreply.github.com>
2022-05-18 14:49:20 +0100

    - modifications for GUI icons

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ build_exe_options = {"packages": ["matplotlib", "pyqtgraph", "vispy", "pyqt5", "
                           os.path.join(python_dir, "vcruntime140.dll"),
                           os.path.join(python_dir, "python37.dll"),
                           "vispy_pyqt_gui/images"],
-                     "build_exe": "Hyve GUI",
+                     "build_exe": "GUI",
                      'path': [os.path.abspath('vispy_pyqt_gui')] + sys.path}
 
 # base="Win32GUI" should be used only for Windows GUI app
@@ -24,12 +24,12 @@ if sys.platform == "win32":
     base = "Win32GUI"
 
 setup(
-    name = "HyveGUI",
+    name = "GUI",
     version = "0.1",
     description = "Data visualisation tool",
     options = {"build_exe": build_exe_options},
     executables = [Executable(r"vispy_pyqt_gui/main.py",
                               base=base,
-                              icon="vispy_pyqt_gui/images/hyve_icon.ico",
-                              targetName="Hyve_GUI")]
+                              icon="vispy_pyqt_gui/images/icon.ico",
+                              targetName="GUI")]
 )
